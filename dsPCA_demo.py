a92e7c8ee4e18aa0139679b589a12478e025ddc7
lucidian1 <lucidian.s@gmail.com>
2022-03-20 17:28:34 +0900

    Ian Updates and demo

---
+++ dsPCA_demo.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 20 15:22:56 2022
+
+@author: lucid
+"""
+
+from dspca.dspca import dsPCA
+from sklearn.decomposition import PCA
+import numpy as np
+import matplotlib
+from matplotlib import pyplot as plt
+from dspca.utils import adjust_lightness
+
+with np.load(r'C:\Users\lucid\OneDrive\Documents\GitHub\dspca\data\data.npz') as data:
+    dQ = data['dQ']  # Target
+    Qch = data['Qch']  # Target
+    sQ = data['sQ']  # Target
+    activity = data['activity']  # Neural population activity (Trial X Time X Cell)
+    
+    targets = np.vstack((dQ, Qch, sQ)).T    # Target task-related variables
+time_range = np.arange(10, 15)  # Time range used to identify subspaces
+activity_mean = np.mean(activity[:, time_range, :], axis=1)  # Temporally averaged neural population activity (Trial X Cell)
+
+projection_target_subspace, projection_targetfree_subspace, ax_targets, ax_targetfree, \
+target_subspace_signal, targetfree_subspace_signal, target_subspace_var, targetfree_subspace_var, total_var, dot_target_ax\
+    = dsPCA(data=activity_mean, targets=targets)
