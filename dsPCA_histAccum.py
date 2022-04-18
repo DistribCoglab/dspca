76dffaedacd4c6cc0866a613187b29f4759c42b4
lucidian1 <lucidian.s@gmail.com>
2022-04-18 18:57:34 +0900

    Update dsPCA_histAccum.py

--- dsPCA_histAccum.py
+++ dsPCA_histAccum.py
@@ -10,10 +10,11 @@ from sklearn.decomposition import PCA
 import numpy as np
 import matplotlib
 import csv
+import scipy.io as sio
 from matplotlib import pyplot as plt
 from dspca.utils import adjust_lightness
 
-file = open("Salary_Data.csv")
+file = open(r'C:\Users\lucid\OneDrive\Documents\GitHub\computeTopLevelScripts\Proj\dimensionRed\dsPCA\Data\Mouse1S1_activity.csv')
 csvreader = csv.reader(file)
 header = next(csvreader)
 print(header)
@@ -23,6 +24,9 @@ for row in csvreader:
 print(rows)
 file.close()
 
+with open(r'C:\Users\lucid\OneDrive\Documents\GitHub\computeTopLevelScripts\Proj\dimensionRed\dsPCA\Data\Mouse1S1_activity.csv') as file_name:
+    array = np.loadtxt(file_name, delimiter=",")
+
 
 with np.load(r'C:\Users\lucid\OneDrive\Documents\GitHub\dspca\data\data.npz') as data:
     dQ = data['dQ']  # Target
@@ -30,10 +34,10 @@ with np.load(r'C:\Users\lucid\OneDrive\Documents\GitHub\dspca\data\data.npz') as
     sQ = data['sQ']  # Target
     activity = data['activity']  # Neural population activity (Trial X Time X Cell)
     
-    targets = np.vstack((dQ, Qch, sQ)).T    # Target task-related variables
-time_range = np.arange(10, 15)  # Time range used to identify subspaces
-activity_mean = np.mean(activity[:, time_range, :], axis=1)  # Temporally averaged neural population activity (Trial X Cell)
+#     targets = np.vstack((dQ, Qch, sQ)).T    # Target task-related variables
+# time_range = np.arange(10, 15)  # Time range used to identify subspaces
+# activity_mean = np.mean(activity[:, time_range, :], axis=1)  # Temporally averaged neural population activity (Trial X Cell)
 
-projection_target_subspace, projection_targetfree_subspace, ax_targets, ax_targetfree, \
-target_subspace_signal, targetfree_subspace_signal, target_subspace_var, targetfree_subspace_var, total_var, dot_target_ax\
-    = dsPCA(data=activity_mean, targets=targets)
+# projection_target_subspace, projection_targetfree_subspace, ax_targets, ax_targetfree, \
+# target_subspace_signal, targetfree_subspace_signal, target_subspace_var, targetfree_subspace_var, total_var, dot_target_ax\
+#     = dsPCA(data=activity_mean, targets=targets)
